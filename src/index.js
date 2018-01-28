a4533124d61afe35ce6f84651e6d398384257a96
Hua Do Gia Khanh <khanh.hua@axa.com.sg>
2018-01-28 10:48:10 +0800

    Clue boxes

--- src/index.js
+++ src/index.js
@@ -7,16 +7,48 @@ AFRAME.registerComponent('clues-component', {
   schema: {
   },
   init () {
-    clues.forEach(clue => {
-      const entity = document.createElement('a-entity');
-      entity.setAttribute('geometry', `primitive:box;height:0.1;depth:0.1;width:0.1`);
-      entity.setAttribute('position', clue.position);
-      entity.setAttribute('material', 'color:green;');
+    this.hudCamera = document.querySelector('#hudCamera');
 
-      this.el.appendChild(entity);
+    clues.forEach(clue => {
+      const clueBox = document.createElement('a-entity');
+      clueBox.setAttribute('position', clue.position);
+      clueBox.setAttribute('clue-box', `message: "${clue.message}"`);
+
+      this.el.appendChild(clueBox);
     });
+
+    // setTimeout((_this) => {
+    //   _this.openClueBox(_this.el.children[0].components['clue-box']);
+    // }, 5000, this);
   },
   update () {
 
+  },
+  openClueBox (clueBox) {
+    const message = clueBox.getMessage();
+    const text = document.createElement('a-entity');
+    text.setAttribute('position', '0 0.1 -0.5');
+    text.setAttribute('text', `width: 1; align: center; color: #00f900; value: ${message}`);
+    this.hudCamera.appendChild(text);
   }
-})
+});
+
+AFRAME.registerComponent('clue-box', {
+  schema: {
+    message: {
+      type: 'string'
+    }
+  },
+  init () {
+    const box = document.createElement('a-entity');
+    box.setAttribute('geometry', `primitive:box;height:0.1;depth:0.1;width:0.1`);
+    box.setAttribute('material', 'color:green;');
+    this.el.appendChild(box);
+  },
+  update () {
+
+  },
+  getMessage () {
+    return this.data.message;
+  }
+});
