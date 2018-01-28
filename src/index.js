79f0d971f9a1b6713e1f25c2ca71f4d5d6bf8756
Hua Do Gia Khanh <khanh.hua@axa.com.sg>
2018-01-29 01:29:49 +0800

    Make choice

--- src/index.js
+++ src/index.js
@@ -11,9 +11,19 @@ AFRAME.registerComponent('clues-component', {
 
     clues.forEach(clue => {
       const clueBox = document.createElement('a-entity');
-      clueBox.setAttribute('position', clue.position);
-      clueBox.setAttribute('clue-box', `message: "${clue.message}"`);
-
+      const data = {
+        message: `${clue.message}`,
+        attributes: clue.attributes
+      };
+      if (clue.choices) {
+        data.choices = clue.choices.map(choice => ({
+          name: choice.name,
+          attributes: choice.attributes
+        }));
+      }
+      clueBox.setAttribute('position', data.attributes.position);
+      delete data.attributes.position;
+      clueBox.setAttribute('clue-box', data);
       this.el.appendChild(clueBox);
     });
   },
@@ -21,13 +31,20 @@ AFRAME.registerComponent('clues-component', {
 
   },
   openClueBox (clueBox) {
-    Array.from(this.hud.children).forEach(item => item.remove());
-
     const message = clueBox.getMessage();
-    const text = document.createElement('a-entity');
-    text.setAttribute('position', '0 0.1 -0.5');
-    text.setAttribute('text', `width: 1; align: center; color: #00f900; value: ${message}`);
-    this.hud.appendChild(text);
+    let text = this.hud.querySelector('[text]');
+    if (text) {
+      text.setAttribute('text', `width: 1; align: center; color: #00f900; value: ${message}`);
+    } else {
+      text = document.createElement('a-entity');
+      text.setAttribute('position', '0 0.1 -0.5');
+      text.setAttribute('text', `width: 1; align: center; color: #00f900; value: ${message}`);
+      this.hud.appendChild(text);
+    }
+
+    if (clueBox.hasContent()) {
+      clueBox.revealContent();
+    }
   }
 });
 
@@ -35,24 +52,76 @@ AFRAME.registerComponent('clue-box', {
   schema: {
     message: {
       type: 'string'
+    },
+    choices: [
+      {
+        name: 'string',
+        attributes: 'object'
+      }
+    ],
+    revealed: {
+      type: 'boolean'
     }
   },
   init () {
     const box = document.createElement('a-entity');
-    box.setAttribute('geometry', `primitive:box;height:0.1;depth:0.1;width:0.1`);
-    box.setAttribute('material', 'color:green;');
+    box.setAttribute('geometry', `primitive:sphere; radius:1.000`);
+    box.setAttribute('material', `color:white; opacity:0.001`);
     this.el.appendChild(box);
 
     box.addEventListener('click', (evt) => {
       console.log('EVT:', evt);
       this.el.parentNode.components['clues-component'].openClueBox(this);
-    })
+    });
+
+    const shape = document.createElement('a-entity');
+    Object.keys(this.data.attributes).forEach(attribute => {
+      shape.setAttribute(attribute, this.data.attributes[attribute]);
+    });
+    box.appendChild(shape);
   },
   update () {
 
   },
   getMessage () {
     return this.data.message;
+  },
+  hasContent () {
+    return this.data.choices && !!this.data.choices.length;
+  },
+  revealContent() {
+    if (this.data.revealed) {
+      return;
+    }
+    console.info('CONTENT:', this.data.choices);
+    const choiceContainerEl = document.createElement('a-entity');
+
+    const count = this.data.choices.length;
+    const span = (count - 1) * 0.301;
+    this.data.choices.forEach((choice, index) => {
+      const choiceEl = document.createElement('a-entity');
+      Object.keys(choice.attributes).forEach(attribute => {
+        choiceEl.setAttribute(attribute, choice.attributes[attribute]);
+      });
+      choiceEl.setAttribute('position', {
+        x: (index * 0.301)  - (span/2),
+        y: 0.426,
+        z: 0
+      });
+
+      choiceContainerEl.appendChild(choiceEl);
+    });
+
+    const animation = document.createElement('a-animation');
+    animation.setAttribute('attribute','rotation');
+    animation.setAttribute('dur','5000');
+    animation.setAttribute('to','0 360 0');
+    animation.setAttribute('easing','linear');
+    animation.setAttribute('repeat','indefinite');
+    choiceContainerEl.appendChild(animation);
+
+    this.el.appendChild(choiceContainerEl);
+    this.data.revealed = true;
   }
 });
 
@@ -68,7 +137,7 @@ AFRAME.registerComponent('walking-trail-item', {
   init () {
     const marker = document.createElement('a-entity');
     marker.setAttribute('geometry', 'primitive: circle; radius: 0.5');
-    marker.setAttribute('material', 'color: red');
+    marker.setAttribute('material', 'color: red; opacity: 0.16');
     marker.setAttribute('position', '0 0.01 0');
     marker.setAttribute('rotation', '-90 0 0');
 
